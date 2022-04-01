2e881d1f4cf7118a3baa8017a4d90c5cfe9686a3
Feliricki <feliricki01@gmail.com>
2022-03-31 21:11:51 -0400

    Switched docker compose dataabase to mysql

--- backend/database.py
+++ backend/database.py
@@ -1,10 +1,39 @@
 import pymongo
+import mysql.connector as mysql
+import os
 
 mongo_client = pymongo.MongoClient('mongo')
 db = mongo_client["312project"]
 
 users_collection = db["users"]
 users_id_collection = db["users_id"]
+
+# This retrieves the environment variable from the docker compose file
+user = os.getenv('DATABASE_USER')   # This is set to 'Felipe' in the docker compose file for now
+password = os.getenv('DATABASE_PASSWORD')   # 'Gallardo'
+# This connects us to the mysql container database
+db = mysql.connect(
+    host='mysql',
+    user=user,
+    passwd=password,
+    database="CSE312-Project"
+)
+cursor = db.cursor(prepared=True)
+# This create the table with an auto incremented id
+# We can use the 'TEXT' type for string and 'BLOB' to store bytes (for images)
+cursor.execute("""CREATE TABLE IF NOT EXISTS users (
+                      id INT AUTO_INCREMENT PRIMARY KEY,
+                      email TEXT,
+                      username TEXT,
+                      deleted TEXT)
+                      """)
+
+show_databases = "SHOW DATABASES"
+cursor.execute(show_databases)  # Should only display 'CSE312-Project'
+databases = cursor.fetchall()
+for database in databases:
+    print(database)
+
 
 
 def get_next_id():
--- backend/server.py
+++ backend/server.py
@@ -7,31 +7,28 @@ import secrets
 
 
 class MyTCPHandler(socketserver.BaseRequestHandler):
-
     clients = []
 
     # def __init__(self,) -> if state variables desired
 
     def handle(self):
         while True:
-            received_data =  self.request.recv(102400000)
-            
+            received_data = self.request.recv(1024).strip()
+            string_data: str = received_data.decode()
+
             if len(self.data) <= 0:
                 return
-            
+
             print('Length of incoming data: ' + str(len(self.data)))
             print("{} sent:".format(self.client_address[0]))
             print(str(self.data))
             sys.stdout.flush()
             sys.stderr.flush()
-            
+
             self.full_bytes_sent += self.data
             self.iterations += 1
-            
-            
-            
-            
-            
+
+
 if __name__ == "__main__":
     HOST, PORT = '0.0.0.0', 8000
 
